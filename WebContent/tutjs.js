6146e9de222ae8c2f88f0f71ecceeaa55fb82055
Zecknaal <zecknaal@gmail.com>
2016-07-25 08:27:24 -0500

    Move to a more usable state
    fixed tree and context menu
    -initial support for modal dialogs
    -added a lot of bower libs. probably gonna delete these.

--- WebContent/tutjs.js
+++ WebContent/tutjs.js
@@ -1,90 +1,167 @@
-var helloworld = angular.module('HelloWorldApp', ['ui.tree'])
-   helloworld.controller('HelloWorldController',function($scope){
-      $scope.toggle = function (scope) {
-          scope.toggle();
-        };
+function TestDataReader(){
+		this.read = function(){
+    	   autoTestData = 
+    		   [    
+		    	{
+		    		'id' : 1,
+		    		'title': 'Product setup testing',
+		    		'nodes' : 
+		    			[
+		    			 	{
+			    			 	'id' : 11,
+			    			 	'title' : '1A1 Setup Test',
+			    			 	'nodes' : [
+				   		    			 	{
+							    			 	'id' : 111,
+							    			 	'title' : 'Create product 1A1',
+									    		'hexcore': 'gunblade',
+							    			 	'nodes' : []
+						    			 	},
+					    			 	]
+		    			 	},
+   		    			 	{
+			    			 	'id' : 12,
+			    			 	'title' : '1A1 Add Test',
+			    			 	'nodes' : []
+		    			 	},
+		    			 	{
+			    			 	'id' : 13,
+			    			 	'title' : '1A1 Subtract Test',
+			    			 	'nodes' : []
+		    			 	},
+		    			 	{
+			    			 	'id' : 14,
+			    			 	'title' : '1A1 Subtract Test',
+			    			 	'nodes' : []
+		    			 	}
+		    			]
+		    	},
+	            {
+		    		'id' : 2,
+		    		'title': 'Sales Order Create testing',
+		    		'nodes' : 
+		    			[
+		    			]
+	            },
+	            {
+		    		'id' : 3,
+		    		'title': 'Sales Order Maintenance testing',
+		    		'nodes' : 
+		    			[ ]
+	            }
+	       ];
+    	   return autoTestData;
+		}
+}
 
-        $scope.moveLastToTheBeginning = function () {
-          var a = $scope.data.pop();
-          $scope.data.splice(0, 0, a);
-        };
-
-        $scope.newSubItem = function (scope) {
-          var nodeData = scope.$modelValue;
-          nodeData.nodes.push({
-            id: nodeData.id * 10 + nodeData.nodes.length,
-            title: nodeData.title + '.' + (nodeData.nodes.length + 1),
-            nodes: []
-          });
-        };
-
-        $scope.collapseAll = function () {
-          $scope.$broadcast('angular-ui-tree:collapse-all');
-        };
-
-        $scope.expandAll = function () {
-          $scope.$broadcast('angular-ui-tree:expand-all');
-        };
+var autoTestTree = angular.module('AutoTestSetupTreeApp', ['ui.tree', 'ui.bootstrap.contextMenu', 'ui.bootstrap'])
+   autoTestTree.controller('AutoTestSetupTreeController',[ '$scope', function($scope, $uibModal, $log){
+	    $scope.testSetContextMenu = [
+	                                ['Add Test', function($itemScope){
+	                                  alert('Add Test');
+	                                }]
+   	   ];
+	    
+	    $scope.open = function (size) {
+	    	var modalInstance = $uibModal.open({
+	    	      animation: $scope.animationsEnabled,
+	    	      templateUrl: 'myModalContent.html',
+	    	      controller: 'ModalInstanceCtrl',
+	    	      size: size,
+	    	      resolve: {
+	    	        items: function () {
+	    	          return $scope.items;
+	    	        }
+	    	      }
+	    	    });
+	    	
+	    	 modalInstance.result.then(function (selectedItem) {
+	    	      $scope.selected = selectedItem;
+	    	    }, function () {
+	    	      $log.info('Modal dismissed at: ' + new Date());
+	    	    });
+	    }
 	   
-	   $scope.data = [{
-	        'id': 1,
-	        'title': 'node1',
-	        'nodes': [
-	          {
-	            'id': 11,
-	            'title': 'node1.1',
-	            'nodes': [
-	              {
-	                'id': 111,
-	                'title': 'node1.1.1',
-	                'nodes': []
-	              }
-	            ]
-	          },
-	          {
-	            'id': 12,
-	            'title': 'node1.2',
-	            'nodes': []
-	          }
-	        ]
-	      }, {
-	        'id': 2,
-	        'title': 'node2',
-	        'nodrop': true, // An arbitrary property to check in custom template for nodrop-enabled
-	        'nodes': [
-	          {
-	            'id': 21,
-	            'title': 'node2.1',
-	            'nodes': []
-	          },
-	          {
-	            'id': 22,
-	            'title': 'node2.2',
-	            'nodes': []
-	          }
-	        ]
-	      }, {
-	        'id': 3,
-	        'title': 'node3',
-	        'nodes': [
-	          {
-	            'id': 31,
-	            'title': 'node3.1',
-	            'nodes': []
-	          }
-	        ]
-	      }];
+       var a = new TestDataReader( );
+
+       $scope.autoTestData = a.read();
+       
+       $scope.loadOnClick = function(scope){
+    	   scope.remove();
+    	   
+       }
+       
+       $scope.remove = function(scope){
+    	   scope.remove();
+    	   
+       }
+       $scope.buttonClick = function(scope){
+    	   scope.remove();
+    	   
+       }
+	    $scope.testContextMenu = [
+	                                ['Add Test Step', function($itemScope){
+	                                  alert('Add Test');
+	                                }]
+	   ];
+	    
+	    $scope.contextMenu = function(scope){
+	    	return [
+	    	        [scope.$modelValue.title,
+	    	         null],
+	    	        null,
+                    ['Add test step',
+                     $scope.addItem],
+                    ['Delete test',
+                     $scope.remove]
+                   ];
+	    }
+       
+	    $scope.addItem = function(scope){
+	    	var nodeData = scope.$modelValue;
+	    	nodeData.title = 'Ttest';
+	        nodeData.nodes.push({
+	          id: nodeData.id * 10 + nodeData.nodes.length,
+	          title: nodeData.title + '.' + (nodeData.nodes.length + 1),
+	          nodes: []
+	        });
+	    };
+	   $scope.updateCurrentItem = function(scope){
+		   this.currentItem = scope;
+	   }
+	   $scope.hidden = false;
+}]);
+
+var autoTestDetail = angular.module('AutoTestSetupDetailApp', ['ui.bootstrap.contextMenu'])
+	autoTestDetail.controller('AutoTestSetupDetailController',function($scope){
+	    $scope.testSetContextMenu = [
+	                                ['Add Test', function($itemScope){
+	                                  alert('Add Test');
+	                                }]
+	   ];
+	   
+    $scope.params = ['Quantity', 'Product'];
+    $scope.title = 'Parameters';
 });
 
+var modalInstanceControl = angular.module('ModalInstanceControlApp', []).controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
 
-var addnumbers = angular.module('AddNumbers', [])
-	addnumbers.controller('AddNumbersController', function($scope) {
-		$scope.AddNumbers = function() {
-			var a = Number($scope.a || 0);
-			var b = Number($scope.b || 0);
-			$scope.sum = a+b;
-	}
-});
+	  $scope.items = items;
+	  $scope.selected = {
+	    item: $scope.items[0]
+	  };
+
+	  $scope.ok = function () {
+	    $uibModalInstance.close($scope.selected.item);
+	  };
+
+	  $scope.cancel = function () {
+	    $uibModalInstance.dismiss('cancel');
+	  };
+	});
+
 angular.element(document).ready(function() {
-    angular.bootstrap(document, ['AddNumbers', 'HelloWorldApp']);
+	angular.bootstrap(document.getElementById('AutoTestDetail'), ['AutoTestSetupDetailApp']);
+	angular.bootstrap(document.getElementById('AutoTestTree'),   ['AutoTestSetupTreeApp']);
+	angular.bootstrap(document.getElementById('AutoTestTreee'),   ['ModalInstanceControlApp']);
 });
